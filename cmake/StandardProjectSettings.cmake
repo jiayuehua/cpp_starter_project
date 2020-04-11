5f3215afc3348baffaf8154c2d07772864810f72
jiayuehua <yuehwa@qq.com>
2020-04-12 00:26:14 +0800

    Iwyu

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,13 +1,13 @@
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(
-    STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
+    STATUS "Setting build type to 'Debug' as none was specified.")
   set(CMAKE_BUILD_TYPE
-      RelWithDebInfo
-      CACHE STRING "Choose the type of build." FORCE)
+    Debug
+    CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
-                                               "MinSizeRel" "RelWithDebInfo")
+    "MinSizeRel" "RelWithDebInfo")
 endif()
 
 find_program(CCACHE ccache)
@@ -23,8 +23,8 @@ endif()
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 option(ENABLE_IPO
-       "Enable Iterprocedural Optimization, aka Link Time Optimization (LTO)"
-       OFF)
+  "Enable Iterprocedural Optimization, aka Link Time Optimization (LTO)"
+  OFF)
 
 if(ENABLE_IPO)
   include(CheckIPOSupported)
