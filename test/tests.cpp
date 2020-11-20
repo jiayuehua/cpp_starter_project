a916fafe0dd3a99e7802c233e9fba10cbc5982d3
rob <jiayuehua@jd.com>
2020-11-20 11:18:23 +0800

    ruleset config

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
-
+int initial() { return 7; }
+const int hello = 40 + initial();
 unsigned int Factorial(unsigned int number)
 {
   return number <= 1 ? number : Factorial(number - 1) * number;
